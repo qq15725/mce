e1d28fa914837f4036058bd51adf816208be34c6
wxm <157215725@qq.com>
2025-10-28 16:49:26 +0800

    fix

--- packages/mce/src/utils/console.ts
+++ packages/mce/src/utils/console.ts
@@ -1,23 +1,9 @@
-// Utilities
 import { warn } from 'vue'
 
 export function consoleWarn(message: string): void {
-  warn(`MCE: ${message}`)
+  warn(`mce: ${message}`)
 }
 
 export function consoleError(message: string): void {
-  warn(`MCE error: ${message}`)
-}
-
-export function deprecate(original: string, replacement: string | string[]) {
-  replacement = Array.isArray(replacement)
-    ? `${replacement.slice(0, -1).map(s => `'${s}'`).join(', ')} or '${replacement.at(-1)}'`
-    : `'${replacement}'`
-  warn(`[MCE UPGRADE] '${original}' is deprecated, use ${replacement} instead.`)
-}
-export function breaking(original: string, replacement: string) {
-  // warn(`[MCE BREAKING] '${original}' has been removed, use '${replacement}' instead. For more information, see the upgrade guide https://github.com/vuetifyjs/vuetify/releases/tag/v2.0.0#user-content-upgrade-guide`)
-}
-export function removed(original: string) {
-  // warn(`[MCE REMOVED] '${original}' has been removed. You can safely omit it.`)
+  warn(`mce error: ${message}`)
 }
