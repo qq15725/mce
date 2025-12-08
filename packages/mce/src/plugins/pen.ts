4c312150f985fc177341db548ddf9f3737a019c5
wxm <157215725@qq.com>
2025-12-08 18:15:57 +0800

    fix: pen

--- packages/mce/src/plugins/pen.ts
+++ packages/mce/src/plugins/pen.ts
@@ -1,10 +1,12 @@
 import type { Element2D } from 'modern-canvas'
-import { Path2D } from 'modern-path2d'
+import { LineCurve, Path2D, Vector2 } from 'modern-path2d'
+import { watch } from 'vue'
 import { definePlugin } from '../plugin'
 
 declare global {
   namespace Mce {
     interface DrawingTools {
+      pen: []
       pencil: []
     }
   }
@@ -13,11 +15,28 @@ declare global {
 export default definePlugin((editor) => {
   const {
     addElement,
+    renderEngine,
+    drawboardPointer,
+    activeDrawingTool,
+    state,
   } = editor
 
-  let pening = false
   let el: Element2D | undefined
-  let path: Path2D | undefined
+  let currentPath: Path2D | undefined
+  let currentLine: LineCurve | undefined
+
+  const update = () => {
+    if (el && currentPath) {
+      el.shape.paths = [
+        { data: currentPath.toData() },
+      ]
+      const box = currentPath!.getBoundingBox()
+      el.style.left = box.left
+      el.style.top = box.top
+      el.style.width = box.width
+      el.style.height = box.height
+    }
+  }
 
   return {
     name: 'mce:pen',
@@ -25,20 +44,17 @@ export default definePlugin((editor) => {
       {
         name: 'pen',
         handle: (start) => {
-          if (pening) {
-            path!.lineTo(start.x, start.y)
-            path!.moveTo(start.x, start.y)
-            el!.shape.paths = [
-              { data: path!.toData() },
-            ]
-            const box = path!.getBoundingBox()
-            el!.style.left = box.left
-            el!.style.top = box.top
-            el!.style.width = box.width
-            el!.style.height = box.height
+          if (el) {
+            if (currentPath) {
+              currentLine = new LineCurve(
+                new Vector2(start.x, start.y),
+                new Vector2(start.x, start.y),
+              )
+              currentPath.currentCurve.addCurve(currentLine)
+              update()
+            }
           }
           else {
-            pening = true
             el = addElement({
               name: 'pen',
               outline: {
@@ -51,11 +67,32 @@ export default definePlugin((editor) => {
                 inPptIs: 'Shape',
               },
             }, {
-              active: true,
               position: start,
             })
-            path = new Path2D()
-            path.moveTo(start.x, start.y)
+            currentPath = new Path2D()
+            currentLine = new LineCurve(
+              new Vector2(start.x, start.y),
+              new Vector2(start.x, start.y),
+            )
+            currentPath.currentCurve.addCurve(currentLine)
+
+            const onMove = () => {
+              const move = drawboardPointer.value
+              if (currentLine && move) {
+                currentLine.p2.x = move.x
+                currentLine.p2.y = move.y
+                update()
+              }
+            }
+
+            renderEngine.value.on('pointermove', onMove)
+            const stop = watch([state, activeDrawingTool], () => {
+              renderEngine.value.off('pointermove', onMove)
+              stop()
+              el = undefined
+              currentPath = undefined
+              currentLine = undefined
+            })
           }
         },
       },
@@ -90,9 +127,6 @@ export default definePlugin((editor) => {
               el.style.top = box.top
               el.style.width = box.width
               el.style.height = box.height
-            },
-            end: () => {
-              //
             },
           }
         },
