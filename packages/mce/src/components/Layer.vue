5780c50730ad9f792bb764b63d89c6bd57209e30
wxm <157215725@qq.com>
2025-12-30 11:25:14 +0800

    fix: layer text

--- packages/mce/src/components/Layer.vue
+++ packages/mce/src/components/Layer.vue
@@ -96,7 +96,7 @@ const thumbnailName = computed(() => {
         value = t('image')
       }
       else if (node.text.isValid()) {
-        value = node.text.getStringContent()
+        value = node.text.innerText
       }
     }
   }
