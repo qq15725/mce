d18f3f18b9cec60b8a215d800e43c7542ec10094
wxm <157215725@qq.com>
2026-03-11 18:36:29 +0800

    lint: fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,24 @@
 // @ts-check
 import antfu from '@antfu/eslint-config'
 
-export default antfu({ type: 'lib' }, {
-  rules: {
-    'no-console': 'off',
-    'eslint-comments/no-unlimited-disable': 'off',
-    'node/prefer-global/process': 'off',
-    'ts/no-namespace': 'off',
-    'ts/no-unsafe-declaration-merging': 'off',
-    'ts/explicit-function-return-type': 'off',
-    'ts/no-empty-object-type': 'off',
-    'vue/custom-event-name-casing': 'off',
-    'vue/valid-template-root': 'off',
-    'vue/valid-v-slot': 'off',
+export default antfu(
+  {
+    type: 'lib',
+    e18e: false,
   },
-})
+  {
+    rules: {
+      'antfu/consistent-list-newline': 'off',
+      'no-console': 'off',
+      'eslint-comments/no-unlimited-disable': 'off',
+      'node/prefer-global/process': 'off',
+      'ts/no-namespace': 'off',
+      'ts/no-unsafe-declaration-merging': 'off',
+      'ts/explicit-function-return-type': 'off',
+      'ts/no-empty-object-type': 'off',
+      'vue/custom-event-name-casing': 'off',
+      'vue/valid-template-root': 'off',
+      'vue/valid-v-slot': 'off',
+    },
+  },
+)
